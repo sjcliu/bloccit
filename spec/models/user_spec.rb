89564dbac255203faf3ab489a9db68ee8e24d895
Stanly Liu <sjcliu@gmail.com>
2016-02-21 23:34:28 +1100

    checkpoint 45 completed

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "password") }
-  it { is_expected.to have_many(:posts) }
+  let(:user) { build(:user) }
   it { is_expected.to have_many(:comments) }
   it { is_expected.to have_many(:favorites) }
   it { is_expected.to have_many(:votes) }
@@ -33,9 +32,9 @@ RSpec.describe User, type: :model do
   end
 
   describe "Invalid user" do
-    let(:user_with_invalid_name) { User.new(name: "", email: "user@bloccit.com") }
-    let(:user_with_invalid_email) { User.new(name: "Bloccit User", email: "") }
-    let(:user_with_invalid_email_format) { User.new(name: "Bloccit User", email: "invalid_format") }
+    let(:user_with_invalid_name) { build(:user, name: "") }
+    let(:user_with_invalid_email) { build(:user, email: "") }
+    let(:user_with_invalid_email_format) { build(:user, email: "invalid_format") }
 
     it "should be an invalid user due to blank name" do
       expect(user_with_invalid_name).to_not be_valid
@@ -104,6 +103,15 @@ RSpec.describe User, type: :model do
      it "returns the appropriate favorite if it exists" do
        favorite = user.favorites.where(post: @post).create
        expect(user.favorite_for(@post)).to eq(favorite)
+     end
+   end
+
+   describe ".avatar_url" do
+     let(:known_user) { build(:user, email: "blochead@bloc.io") }
+
+     it "returns the proper Gravatar url for a known email entity" do
+       expected_gravatar = "http://gravatar.com/avatar/bb6d1172212c180cfbdb7039129d7b03.png?s=48"
+       expect(known_user.avatar_url(48)).to eq(expected_gravatar)
      end
    end
   end
