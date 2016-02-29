21befa8486f136c4556e677be4a5a4ad7954b364
Stanly Liu <sjcliu@gmail.com>
2016-03-01 00:54:19 +1100

    assignment 45 completed

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -2,4 +2,12 @@ module CommentsHelper
   def user_is_authorized_for_comment?(comment)
     current_user && (current_user == comment.user || current_user.admin?)
   end
+
+  def comments(user)
+    if user.comments.count > 0
+      pluralize(@user.comments.count, 'comment')
+    else
+      "Don't have any comments"
+    end
+  end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+
+
+  def display_posts_count(user)
+    if user.posts.count > 0
+      pluralize(user.posts.count, 'post')
+    else
+      "Ain't got no posts"
+    end
+  end
 end
