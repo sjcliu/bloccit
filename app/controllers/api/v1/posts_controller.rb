b9b1ba39acc6310ab3ea717004813e359798e45d
Stanly Liu <sjcliu@gmail.com>
2016-03-04 20:20:11 +1100

    assignment 49 completed

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::PostsController < Api::V1::BaseController
 
-   before_action :authenticate_user, except: [:index, :show]
-   before_action :authorize_user, except: [:index, :show]
+  #  before_action :authenticate_user, except: [:index, :show]
+  #  before_action :authorize_user, except: [:index, :show]
 
 
    def show
@@ -14,10 +14,29 @@ class Api::V1::PostsController < Api::V1::BaseController
      render json: posts.to_json, status: 200
    end
 
+   def update
+     post = Post.find(params[:id])
+
+     if post.update_attributes(post_params)
+       render json: post.to_json, status: 200
+     else
+       render json: {error: "Post update failed", status: 400}, status: 400
+     end
+   end
+
+   def delete
+     post = Post.find(params[:id])
+
+     if post.delete
+       render json: {message: "Topic deleted", status: 200}, status: 200
+     else
+       render json: {error: "Post delete failed", status: 400}, status: 400
+     end
+   end
 
    private
 
-   def topic_params
-     params.require(:topic).permit(:name, :description, :public)
+   def post_params
+     params.require(:post).permit(:name, :description, :public)
    end
  end
